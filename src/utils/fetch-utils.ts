d8ee45aa556de47e29a21156ffa6cc670f6f15b5
Max <maxsizen@gmail.com>
2019-05-13 11:50:14 +0800

    handle 401 error

--- src/utils/fetch-utils.ts
+++ src/utils/fetch-utils.ts
@@ -11,23 +11,37 @@ const post = (url: string, data: object) => fetch(url, {
   method: 'POST',
   headers: {
     "Content-Type": "application/json",
-    "x-access-token": getToken()
+    "Authorization": `JWT ${getToken()}`
   },
   body: JSON.stringify(data)
-}).then(res => res.json());
+}).then(res => {
+  if (res.status === 401) {
+    return Promise.resolve({ code: 'invalid token' });
+  } else {
+    return res.json();
+  }
+});
 
 const get = (url: string) => fetch(url, {
   method: 'GET',
   headers: {
     // "Content-Type": "application/json",
-    "x-access-token": getToken()
+    "Authorization": `JWT ${getToken()}`
   },
-}).then(res => res.json());
+})
+.then(res => {
+  if (res.status === 401) {
+    return Promise.resolve({ code: 'invalid token' });
+  } else {
+    return res.json();
+  }
+})
+.catch(err => console.log(err))
 
 const getUser = () => {
   try {
-    const obj = JSON.parse(window.localStorage.getItem('token'));
-    return JSON.parse(obj.user);
+    const { user } = JSON.parse(window.localStorage.getItem('token'));
+    return user;
   } catch (error) {
     return {};
   }
